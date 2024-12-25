809ec7e1e244b850130ee65b3129017815421b26
B. Hodes <b.hodes@student.tue.nl>
2024-12-25 10:57:55 +0100

    Day 25 part 1

--- 2024/Day24.py
+++ 2024/Day24.py
@@ -3,47 +3,44 @@ import networkx as nx
 import matplotlib.pyplot as plt
 
 
-def part_a(data):
-    inputs = data.split("\n\n")
-    inputVariables = inputs[0].split("\n")
-    inputLogicGates = inputs[1].split("\n")
+def parse_data(data):
+    sections = data.split("\n\n")
+    variables = [line.split(": ") for line in sections[0].splitlines()]
+    gates = sections[1].splitlines()
+    return variables, gates
 
-    # Create a directed graph
+
+def build_graph(variables, gates):
     G = nx.DiGraph()
 
-    # Add variables to the graph as nodes with no outgoing edges
-    for line in inputVariables:
-        name, value = line.split(":")
-        G.add_node(name, type="variable", value=int(value))
+    # Add variables as nodes
+    for name, value in variables:
+        G.add_node(name, type="variable", operation=None, value=int(value))
 
-    # Add logic gates to the graph as nodes and their dependencies as edges
-    for line in inputLogicGates:
-        parts = line.split(" -> ")
-        gate = parts[0]
-        target = parts[1]
-
-        if "AND" in gate:
-            a, b = gate.split(" AND ")
+    # Add gates and connections
+    for gate in gates:
+        logic, target = gate.split(" -> ")
+        if "AND" in logic:
+            a, b = logic.split(" AND ")
             G.add_node(target, type="gate", operation="AND")
             G.add_edge(a, target)
             G.add_edge(b, target)
-        elif "XOR" in gate:
-            a, b = gate.split(" XOR ")
+        elif "XOR" in logic:
+            a, b = logic.split(" XOR ")
             G.add_node(target, type="gate", operation="XOR")
             G.add_edge(a, target)
             G.add_edge(b, target)
-        elif "OR" in gate:
-            a, b = gate.split(" OR ")
+        elif "OR" in logic:
+            a, b = logic.split(" OR ")
             G.add_node(target, type="gate", operation="OR")
             G.add_edge(a, target)
             G.add_edge(b, target)
 
-    # nx.draw(G, with_labels=True)
-    # plt.show()
+    return G
 
-    # Evaluate the graph using topological sort
+
+def evaluate_graph(G):
     for node in nx.topological_sort(G):
-        print(node)
         if G.nodes[node]["type"] == "gate":
             a, b = list(G.predecessors(node))
             operation = G.nodes[node]["operation"]
@@ -54,64 +51,117 @@ def part_a(data):
             elif operation == "OR":
                 G.nodes[node]["value"] = G.nodes[a]["value"] | G.nodes[b]["value"]
 
+
+def get_output(G):
     # Get all variables starting with z
     keys = [key for key in G.nodes if key.startswith("z")]
     keys.sort(reverse=True)
     result = "".join([str(G.nodes[key]["value"]) for key in keys])
     # Convert binary to decimal
-    result = int(result, 2)
-    return result
+    return int(result, 2)
+
+
+def set_input(G, x, y):
+    # x and y are 45 bit wide
+    x = format(x, "045b")
+    y = format(y, "045b")
+    for i in range(45):
+        G.nodes[f"x{i:02}"]["value"] = int(x[i])
+        G.nodes[f"y{i:02}"]["value"] = int(y[i])
+    return
+
+
+def check_Valid(G):
+    for x in range(128):
+        for y in range(128):
+            set_input(G, x, y)
+            evaluate_graph(G)
+            if get_output(G) != x + y:
+                print(f"Error at {x} + {y}")
+                print(f"Output: {get_output(G)} but should be {x+y}")
+                return False
+    return True
+
+
+def part_a(data):
+    variables, gates = parse_data(data)
+
+    G = build_graph(variables, gates)
+    evaluate_graph(G)
+    get_output(G)
+    return get_output(G)
+
+
+def part_b(data):
+    variables, gates = parse_data(data)
+    G = build_graph(variables, gates)
+
+    valid = False
+    while not valid:
+        valid = check_Valid(G)
+
+        swaplist = []
+        for node in G.nodes:
+            foundError = False
+            if node.startswith("z"):
+                if G.nodes[node]["operation"] != "XOR":
+                    print(
+                        f"Wrong connection {node}: {G.nodes[node]['operation'] } Should be XOR"
+                    )
+                    swaplist.append(node)
+                    continue
+
+                nodes = list(G.predecessors(node))
+                XOR_Gate = None
+                OR_Gate = None
+                for node in nodes:
+                    print(XOR_Gate, OR_Gate)
+
+                    if not XOR_Gate and G.nodes[node]["operation"] == "XOR":
+                        XOR_Gate = node
+                    elif not OR_Gate and G.nodes[node]["operation"] == "OR":
+                        OR_Gate = node
+                    else:
+                        print(f"Wrong connection {node}: {G.nodes[node]['operation'] }")
+                        swaplist.append(node)
+                        foundError = True
+
+                if foundError:
+                    continue
+
+                if XOR_Gate:
+                    for node in G.predecessors(XOR_Gate):
+                        if node.startswith("x"):
+                            continue
+                        elif node.startswith("y"):
+                            continue
+                        else:
+                            print(
+                                f"Wrong connection {node}: {G.nodes[node]['operation'] } Shoulde be x of y"
+                            )
+                            swaplist.append(node)
+                            foundError = True
+
+                if foundError:
+                    continue
+
+                if OR_Gate:
+                    for node in G.predecessors(OR_Gate):
+                        if G.nodes[node]["operation"] == "AND":
+                            continue
+                        else:
+                            print(
+                                f"Wrong connection {node}: {G.nodes[node]['operation'] } Should be AND"
+                            )
+                            swaplist.append(node)
+
+        print(swaplist)
+        print(len(swaplist))
+        quit()
 
 
 if __name__ == "__main__":
     puzzle = Puzzle(year=2024, day=24)
 
-    data = """x00: 1
-x01: 0
-x02: 1
-x03: 1
-x04: 0
-y00: 1
-y01: 1
-y02: 1
-y03: 1
-y04: 1
-
-ntg XOR fgs -> mjb
-y02 OR x01 -> tnw
-kwq OR kpj -> z05
-x00 OR x03 -> fst
-tgd XOR rvg -> z01
-vdt OR tnw -> bfw
-bfw AND frj -> z10
-ffh OR nrd -> bqk
-y00 AND y03 -> djm
-y03 OR y00 -> psh
-bqk OR frj -> z08
-tnw OR fst -> frj
-gnj AND tgd -> z11
-bfw XOR mjb -> z00
-x03 OR x00 -> vdt
-gnj AND wpb -> z02
-x04 AND y00 -> kjc
-djm OR pbm -> qhw
-nrd AND vdt -> hwm
-kjc AND fst -> rvg
-y04 OR y02 -> fgs
-y01 AND x02 -> pbm
-ntg OR kjc -> kwq
-psh XOR fgs -> tgd
-qhw XOR tgd -> z09
-pbm OR djm -> kpj
-x03 XOR y03 -> ffh
-x00 XOR y04 -> ntg
-bfw OR bqk -> z06
-nrd XOR fgs -> wpb
-frj XOR qhw -> z04
-bqk OR frj -> z07
-y03 OR x01 -> nrd
-hwm AND bqk -> z03
-tgd XOR rvg -> z12
-tnw OR pbm -> gnj"""
-    print(part_a(data))
-    puzzle.answer_a = part_a(puzzle.input_data)
+    # puzzle.answer_a = part_a(puzzle.input_data)
+    print(part_b(puzzle.input_data))
---
+++ 2024/Day25.py
@@ -0,0 +1,35 @@
+from aocd.models import Puzzle
+
+
+def part_a(data):
+    schematics = data.split("\n\n")
+
+    keys = []
+    locks = []
+
+    for schematic in schematics:
+        # Count number of # in each column
+        columns = [-1] * len(schematic.splitlines()[0])
+        for line in schematic.splitlines():
+            for i, char in enumerate(line):
+                if char == "#":
+                    columns[i] += 1
+        print(schematic[0][0])
+        if schematic[0][0] == "#":
+            locks.append(columns)
+        else:
+            keys.append(columns)
+
+    # Check which keys and locks fit together
+    count = 0
+    for key in keys:
+        for lock in locks:
+            temp = [lock[i] + key[i] for i in range(len(lock))]
+            if max(temp) <= 5:
+                count += 1
+    return count
+
+
+if __name__ == "__main__":
+    puzzle = Puzzle(year=2024, day=25)
+    puzzle.answer_a = part_a(puzzle.input_data)
